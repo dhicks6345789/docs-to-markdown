f19e2f96da5872742e21dc0d2c57163f38358698
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-04-04 18:02:42 +0100

    Update docsToMarkdown.py

--- docsToMarkdown.py
+++ docsToMarkdown.py
@@ -97,6 +97,7 @@ def documentToGovspeak(inputFile):
     # As of around Monday, 4th March 2019, Pandoc 2.7 now seems to work correctly for parsing DOCX files produced by Word Online.
     # Debian's Pandoc package is still on version 2.5, so Pandoc needs to be installed via the .deb file provided on their website.
     # This proved to be a simple enough install, no problems.
+    print("pandoc --wrap=none -s " + inputFile + " -t gfm -o -")
     pandocProcess = subprocess.Popen("pandoc --wrap=none -s " + inputFile + " -t gfm -o -", shell=True, stdout=subprocess.PIPE)
     for markdownLine in pandocProcess.communicate()[0].decode("utf-8").split("\n"):
         lineIsFrontMatter = False
