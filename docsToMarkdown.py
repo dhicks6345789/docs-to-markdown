d2120a45201d079c63b66e8226ef12d7f018c3fa
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-10-22 15:03:03 +0100

    Update docsToMarkdown.py

--- docsToMarkdown.py
+++ docsToMarkdown.py
@@ -383,6 +383,5 @@ for userFunction in userFunctions:
         for folderToProcess in foldersToProcess:
             userFolderMatchResult = re.match(userFunction["inputFolder"], folderToProcess)
             if not userFolderMatchResult == None:
-                print("Folder to process: " + folderToProcess[len(args["input"]):])
-                outputFile = args["data"] + os.sep + re.sub(userFunction["inputFolder"], userFunction["outputFile"], folderToProcess[len(args["input"]):])
+                outputFile = args["data"] + re.sub(userFunction["inputFolder"], userFunction["outputFile"], folderToProcess[len(args["input"]):])
                 print("List files in " + folderToProcess + " to " + outputFile, flush=True)
