94a6837064d866a9d4e5d13f66dd7062f77a9b80
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-10-23 12:54:08 +0100

    Update docsToMarkdown.py

--- docsToMarkdown.py
+++ docsToMarkdown.py
@@ -288,7 +288,7 @@ if "config" in args.keys():
 			args[argsDataValues[0]] = cellToStr(argsDataValues[1])
 		elif argsDataValues[0] in optionalLists:
 			for argsDataValue in argsDataValues[1:].values:
-				if not argsDataValue == numpy.nan:
+				if not math.isnan(argsDataValue):
 					args[argsDataValues[0]].append(argsDataValue)
 		elif argsDataValues[0] in functionArgs.keys():
 			userFunction = {}
