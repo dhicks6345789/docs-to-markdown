cdbf94c1dfe657337570babb99bf82f05f9d2e01
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-02-10 18:51:43 +0000

    Update scanFolders.py

--- scanFolders.py
+++ scanFolders.py
@@ -1,14 +1,14 @@
 import os
+import sys
+
+# We use the Pandas library, which in turn uses the XLRD library, to read Excel data.
+import xlrd
+import pandas
 
 args = {}
+requiredArgs = ["input","output"]
+optionalArgs = []
 args["input"] = "."
-args["output"] = ""
-
-# When we examine a Word document, if it starts with what looks like YAML-style variables then we will treat that as Hugo / Jeykll
-# front matter values. We only check for the variables as given below, otherwise every document that starts with a colon in the
-# first line would get treated as front matter.
-args["validFrontMatterFields"] = ["title","lastUpdated"]
-#args["defaultFrontMatter"] = {"layout":"default"}
 
 # Process the command-line arguments.
 currentArgName = None
@@ -17,6 +17,7 @@ for argItem in sys.argv[1:]:
         currentArgName = argItem[2:]
     elif not currentArgName == None:
         args[currentArgName] = argItem
+        print(currentArgName + ": " + argItem)
         currentArgName = None
     else:
         print("ERROR: unknown argument, " + argItem)
@@ -30,15 +31,10 @@ if "config" in args.keys():
     for argsDataIndex, argsDataValues in argsData.iterrows():
         if argsDataValues[0] in requiredArgs + optionalArgs:
             args[argsDataValues[0]] = valueToString(argsDataValues[1])
-        elif argsDataValues[0] in optionalLists:
-            for argsDataValue in argsDataValues[1:].values:
-                if not isnan(argsDataValue):
-                    args[argsDataValues[0]].append(argsDataValue)
-        elif argsDataValues[0] in functionArgs.keys():
-            userFunction = {}
-            userFunction["function"] = argsDataValues[0]
-            functionArgIndex = 1
-            for functionArg in functionArgs[argsDataValues[0]]:
-                userFunction[functionArg] = argsDataValues[functionArgIndex]
-                functionArgIndex = functionArgIndex + 1
-            userFunctions.append(userFunction)
+            print(argsDataValues[0] + ": " + args[argsDataValues[0]])
+            
+for requiredArg in requiredArgs:
+    if not requiredArg in args.keys():
+        print("ERROR: Missing value for argument " + requiredArg)
+        print("Usage: scanFolders --config --input --output")
+        sys.exit(1)
