4bad4ce7bbde330c6afdb342ad86ae54351cf66c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-02-10 23:20:23 +0000

    Update scanFolders.py

--- scanFolders.py
+++ scanFolders.py
@@ -28,6 +28,7 @@ def scanFolder(theInput, theOutput):
                 if not re.match(match[0], theInput + os.sep + item + os.sep) == None:
                     commandLine = match[1] + " " + inputFolder + os.sep + item + " " + normalisePath(baseOutput + os.sep + theOutput + os.sep + item)
                     print("Running: " + commandLine)
+                    os.system(commandLine)
     for item in os.listdir(inputFolder):
         if os.path.isdir(inputFolder + os.sep + item):
             scanFolder(normalisePath(theInput + os.sep + item), normalisePath(theOutput + os.sep + item))
