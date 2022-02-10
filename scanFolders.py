732509737cd2af47aefdb8e11203fd9eaf684855
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-02-10 20:14:57 +0000

    Update scanFolders.py

--- scanFolders.py
+++ scanFolders.py
@@ -25,14 +25,14 @@ def scanFolder(theInput, theOutput):
         print("Is folder?: " + inputFolder + os.sep + item)
         if os.path.isdir(inputFolder + os.sep + item):
             for match in matches:
-                print("Does " + match[0] + " match " + theInput)
-                if not re.match(match[0], theInput) == None:
+                print("Does " + match[0] + "/ match " + theInput + "/")
+                if not re.match(match[0], theInput + "/") == None:
                     print("Match - path: " + inputFolder + os.sep + item + " matches " + match[0])
                     commandLine = match[1] + inputFolder + os.sep + item + " " + normalisePath(baseOutput + os.sep + theOutput + os.sep + item)
                     print("Running: " + commandLine)
     for item in os.listdir(inputFolder):
         if os.path.isdir(inputFolder + os.sep + item):
-            scanFolder(theInput + os.sep + item, theOutput + os.sep + item)
+            scanFolder(normalisePath(theInput + os.sep + item), normalisePath(theOutput + os.sep + item))
 
 # Process the command-line arguments.
 currentArgName = None
