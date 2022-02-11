87419d5d8fd0018875dd93b1618a3b506e00c97f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-02-11 19:16:40 +0000

    Update processFAQ.py

--- processFAQ.py
+++ processFAQ.py
@@ -5,6 +5,6 @@ inputFolder = sys.argv[1]
 outputFolder = sys.argv[2]
 
 print("processFAQ: " + inputFolder + " to " + outputFolder)
-for item is os.listdir(inputFolder):
+for item in os.listdir(inputFolder):
     if item.rsplit(".", 1)[1].lower() in ["docx", "doc"]:
         print("Convert to Markdown: " + inputFolder + os.sep + item)
