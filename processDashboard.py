fdfe1bfe683acfa2ef69d0786c8bd6e2b643682f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-02-23 14:19:38 +0000

    Update processDashboard.py

--- processDashboard.py
+++ processDashboard.py
@@ -70,9 +70,10 @@ def newRow():
     rowHeight = 1
     rowItems = []
 
-print(sections)
 for section in sections:
     if not section[1] == {}:
+        if not section[0] == inputFolder:
+            rowTitle = section[0][len(inputFolder):]
         for fileName in section[1].keys():
             for fileType in section[1][fileName]:
                 fullPath = section[0] + os.sep + fileName + "." + fileType
