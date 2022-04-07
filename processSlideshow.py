3ccd69629fddfc4a4c0d11b33dd6c137f436c059
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-04-07 11:49:12 +0100

    Update processSlideshow.py

--- processSlideshow.py
+++ processSlideshow.py
@@ -85,6 +85,8 @@ for slide in slides:
             SVGContent = docsToMarkdownLib.embedBitmapInSVG(inputFolder + os.sep + slide + "." + fileType, args["width"], args["height"])
             docsToMarkdownLib.putFile(args["output"] + os.sep + str(slideCount) + ".svg", SVGContent)
             slideList.append(str(slideCount) + ".svg")
+        elif fileType in docsToMarkdownLib.videoTypes:
+            thumbnailImage(inputFolder + os.sep + slide + "." + fileType, args["output"] + os.sep + str(slideCount) + ".mp4", args["width"], args["height"])
         else:
             shutil.copyfile(inputFolder + os.sep + slide + "." + fileType, args["output"] + os.sep + str(slideCount) + "." + fileType.lower())
             slideList.append(str(slideCount) + "." + fileType.lower())
