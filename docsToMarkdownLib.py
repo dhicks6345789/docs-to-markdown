ff4682b4933698d4c22a4dda3fb74bb285000890
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-04-07 15:46:57 +0100

    Update docsToMarkdownLib.py

--- docsToMarkdownLib.py
+++ docsToMarkdownLib.py
@@ -195,11 +195,11 @@ def thumbnailVideo(theInputVideo, theOutputVideo, theBlockWidth, theBlockHeight)
     if pictureRatio < outputRatio:
         padHeightRatio = 1 + (outputRatio - pictureRatio)
         resultHeight = int(videoHeight / padHeightRatio)
-        pasteX = (resultWidth - int(videoWidth / padHeightRatio)) / 2
+        pasteX = int((resultWidth - int(videoWidth / padHeightRatio)) / 2)
     elif pictureRatio > outputRatio:
         padWidthRatio = 1 + (pictureRatio - outputRatio)
         resultWidth = int(videoWidth / padWidthRatio)
-        pasteX = (resultHeight - int(videoHeight / padWidthRatio)) / 2
+        pasteX = int((resultHeight - int(videoHeight / padWidthRatio)) / 2)
     
     ffmpegLine = "ffmpeg -i \"" + theInputVideo + "\" -vf \"pad=" + str(resultWidth) + ":" + str(resultHeight) + ":" + str(pasteX) + ":" + str(pasteY) + "\" \"" + theOutputVideo + "\" 2>&1"
     print(ffmpegLine)
