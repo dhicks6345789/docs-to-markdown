c67303712c5c07950096e1dbc85b22a18bf7f255
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-02-15 14:46:53 +0000

    Update processDashboard.py

--- processDashboard.py
+++ processDashboard.py
@@ -43,7 +43,7 @@ print("STATUS: processDashboard: " + args["input"] + " to " + args["output"], fl
 os.makedirs(args["output"], exist_ok=True)
 
 # Copy files to output from generator folder.
-shutil.copytree(args["input"] + "/" args["generator"], args["output"] + "/" args["generator"], copy_function=copyIfNew, dirs_exist_ok=True)
+shutil.copytree(args["input"] + "/" + args["generator"], args["output"] + "/" + args["generator"], copy_function=copyIfNew, dirs_exist_ok=True)
 
 def noBlank(theValue, theDefault):
     if str(theValue).strip() == "":
