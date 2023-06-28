5116a8c1f05694f5fe4babaca204067cb855d371
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-06-28 13:26:16 +0100

    Update processSlideshow.py

--- processSlideshow.py
+++ processSlideshow.py
@@ -13,6 +13,7 @@ import docsToMarkdownLib
 args = docsToMarkdownLib.processCommandLineArgs(defaultArgs={"width":"16", "height":"9"}, requiredArgs=["input","output"])
 
 print("STATUS: processSlideshow: " + args["input"] + " to " + args["output"], flush=True)
+print("STATUS: Bananas!")
 
 # Make sure the output folder exists.
 os.makedirs(args["output"], exist_ok=True)
