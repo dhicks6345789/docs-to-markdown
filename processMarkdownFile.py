fc1a82352aa5bd87ff2c6e1f7c3875135b3246f6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-01-11 13:19:59 +0000

    Update processMarkdownFile.py

--- processMarkdownFile.py
+++ processMarkdownFile.py
@@ -1,1 +1,10 @@
-print("Processing Markdown file: " + "Bananas")
+import os
+import sys
+
+# Our own Docs To Markdown library.
+import docsToMarkdownLib
+
+inputFile = sys.argv[1]
+outputFolder = sys.argv[2]
+
+print("STATUS: processMarkdownFile: " + inputFile + " to " + outputFolder)
