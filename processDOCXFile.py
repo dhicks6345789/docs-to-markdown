46f9af8b00f21fda76898bf9ccd52dc88234d574
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-01-11 14:13:03 +0000

    Update processDOCXFile.py

--- processDOCXFile.py
+++ processDOCXFile.py
@@ -7,4 +7,20 @@ import docsToMarkdownLib
 inputFile = sys.argv[1]
 outputFolder = sys.argv[2]
 
-print("STATUS: processMarkdownFile: " + inputFile + " to " + outputFolder)
+if inputFile.rsplit(".", 1)[1].lower() in ["docx", "doc"]:
+  outputFile = inputFile
+  if os.sep in outputFile:
+    outputFile = outputFile.rsplit(os.sep, 1)[1]
+  outputFile = outputFile.rsplit(".", 1)[0] + ".md"
+  outputPath = outputFolder + os.sep + outputFile
+  if not docsToMarkdownLib.checkModDatesMatch(inputFile, outputPath):
+    print("STATUS: Processing DOCX file: " + inputFile + " to " + outputPath, flush=True)
+    docMarkdown, docFrontmatter = docsToMarkdownLib.docToMarkdown(inputFile)
+    trimmedMarkdown = ""
+    for markdownLine in docMarkdown.split("\n"):
+      if markdownLine.startswith("# ") and not "title" in docFrontmatter.keys():
+        docFrontmatter["title"] = markdownLine[2:].lstrip()
+      else:
+        trimmedMarkdown = trimmedMarkdown + markdownLine + "\n"
+    docsToMarkdownLib.putFile(outputPath, docsToMarkdownLib.frontMatterToString(docFrontmatter) + trimmedMarkdown.strip())
+    docsToMarkdownLib.makeModDatesMatch(inputFile, outputPath)
