7ca0196d7edab0168458633bce0fb03bba14fa9a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-07-05 14:03:00 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,23 @@
 @echo off
 echo Installing DocsToMarkdown as a Windows service...
 
-rem Stop any existing running service.
-net stop DocsToMarkdown > nul 2>&1
+rem Parse any parameters.
+if "%1"=="" (
+  echo Installs DocsToMarkdown as a Windows periodic task.
+  echo Usage: install PathToDocsToMarkdownExecutable
+  goto end
+)
+set docsToMarkdownPath=%1
 
-NSSM\2.24\win64\nssm install DocsToMarkdown "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
-NSSM\2.24\win64\nssm set DocsToMarkdown DisplayName "Docs To Markdown" > nul 2>&1
-NSSM\2.24\win64\nssm set DocsToMarkdown AppNoConsole 1 > nul 2>&1
-NSSM\2.24\win64\nssm set DocsToMarkdown Start SERVICE_AUTO_START > nul 2>&1
-net start DocsToMarkdown
+schtasks /query /tn DocsToMarkdown
+
+rem rem Stop any existing running service.
+rem net stop DocsToMarkdown > nul 2>&1
+
+rem NSSM\2.24\win64\nssm install DocsToMarkdown "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
+rem NSSM\2.24\win64\nssm set DocsToMarkdown DisplayName "Docs To Markdown" > nul 2>&1
+rem NSSM\2.24\win64\nssm set DocsToMarkdown AppNoConsole 1 > nul 2>&1
+rem NSSM\2.24\win64\nssm set DocsToMarkdown Start SERVICE_AUTO_START > nul 2>&1
+rem net start DocsToMarkdown
+
+:end
