c96d6ee0588880cb5023978bb462d1ba2583f189
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-07-05 14:29:30 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,7 +10,7 @@ if [%1]==[] (
 set docsToMarkdownPath=%1
 
 schtasks /query /tn DocsToMarkdown 2>&1 | find /i "ERROR: The system cannot find the file specified."
-if errorlevel 1 (
+if not errorlevel 1 (
   echo DocsToMarkdown Task not present.
 )
 
