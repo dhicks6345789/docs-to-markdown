bbd13f625fabe19cbcc95a683533a0ec6a6ed938
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-07-05 14:38:36 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,8 +10,10 @@ if [%1]==[] (
 set docsToMarkdownPath=%1
 
 schtasks /query /tn DocsToMarkdown 2>&1 | find /i "ERROR: The system cannot find the file specified." >NUL
-if not errorlevel 1 (
-  echo DocsToMarkdown Task not present.
+if errorlevel 1 (
+  echo DocsToMarkdown Task present.
+  rem TODO: Remove existing schedualed task.
 )
+rem TODO: Add new schedualed task, pointing at WEbconsole URL.
 
 :end
