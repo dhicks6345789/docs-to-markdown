30bdcb40f304c48449557587b15f899031cc6736
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-07-05 14:24:13 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -9,15 +9,9 @@ if [%1]==[] (
 )
 set docsToMarkdownPath=%1
 
-schtasks /query /tn DocsToMarkdown
-
-rem rem Stop any existing running service.
-rem net stop DocsToMarkdown > nul 2>&1
-
-rem NSSM\2.24\win64\nssm install DocsToMarkdown "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
-rem NSSM\2.24\win64\nssm set DocsToMarkdown DisplayName "Docs To Markdown" > nul 2>&1
-rem NSSM\2.24\win64\nssm set DocsToMarkdown AppNoConsole 1 > nul 2>&1
-rem NSSM\2.24\win64\nssm set DocsToMarkdown Start SERVICE_AUTO_START > nul 2>&1
-rem net start DocsToMarkdown
+schtasks /query /tn DocsToMarkdown 2>&1 | find /i "ERROR: The system cannot find the file specified."
+if errorlevel 1 (
+  echo DocsToMarkdown Task not present.
+)
 
 :end
