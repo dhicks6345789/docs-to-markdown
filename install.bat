e06139ceab54728e0dde5236ddfd5b58005d55e9
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-07-05 13:52:24 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,11 @@
 @echo off
-echo Installing DocsToMarkdown as a Wi ndows service...
+echo Installing DocsToMarkdown as a Windows service...
 
-rem Stop any existing running services.
+rem Stop any existing running service.
 net stop DocsToMarkdown > nul 2>&1
 
-echo Setting up WebConsole as a Windows service...
-NSSM\2.24\win64\nssm install WebConsole "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
-NSSM\2.24\win64\nssm set WebConsole DisplayName "Web Console" > nul 2>&1
-NSSM\2.24\win64\nssm set WebConsole AppNoConsole 1 > nul 2>&1
-NSSM\2.24\win64\nssm set WebConsole Start SERVICE_AUTO_START > nul 2>&1
+NSSM\2.24\win64\nssm install DocsToMarkdown "C:\Program Files\WebConsole\webconsole.exe" > nul 2>&1
+NSSM\2.24\win64\nssm set DocsToMarkdown DisplayName "Docs To Markdown" > nul 2>&1
+NSSM\2.24\win64\nssm set DocsToMarkdown AppNoConsole 1 > nul 2>&1
+NSSM\2.24\win64\nssm set DocsToMarkdown Start SERVICE_AUTO_START > nul 2>&1
 net start DocsToMarkdown
