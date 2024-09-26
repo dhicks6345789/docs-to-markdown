d5ff761c18adc462475a59c87491e78aea515b63
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-09-26 13:14:33 +0100

    Update processMailmerge.py

--- mailmerge/processMailmerge.py
+++ mailmerge/processMailmerge.py
@@ -7,7 +7,7 @@ import os
 # Importantly, when installing with pip, that's not the "docx" library, that's an earlier version - do "pip install python-docx"...
 import docx
 # ...and the docs-replace library, to install do ""pip install python-docx-replace".
-import docx_replace
+import python_docx_replace 
 
 # We use Pandas to import Excel / CSV files for configuration details.
 import pandas
