2b29bca63e982635ffcc13ca19611f48dca9e3d1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-07-02 13:40:21 +0100

    Update processStartScreen.py

--- startScreen/processStartScreen.py
+++ startScreen/processStartScreen.py
@@ -78,7 +78,7 @@ for dataTuple in dataTuples:
             if bestFavicon:
                 print("Best favicon URL:" + bestFavicon.url, flush=True)
                 print("Favicon dimensions:" + str(bestFavicon.width) + "x" + str(bestFavicon.height), flush=True)
-                print(bestFavicon.url, bestFavicon.valid, bestFavicon.width, bestFavicon.height, flush=True)
+                print(bestFavicon.url, bestFavicon.valid, bestFavicon.width, bestFavicon.height, bestFavicon.image, flush=True)
             else:
                 print("No valid favicon found for this URL.", flush=True)
         resourceTable.append([URL, title, description, icon])
