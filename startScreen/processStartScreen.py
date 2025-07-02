4d8b3071e31d5e25f656acc4267a478baf4f681f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-07-02 14:31:35 +0100

    Update processStartScreen.py

--- startScreen/processStartScreen.py
+++ startScreen/processStartScreen.py
@@ -30,6 +30,22 @@ def itemOrBlank(theRow, theIndex):
         return theRow[theIndex]
     return ""
 
+# Generate a simple "cyrb53" hash from a given string. Non-cryptographic, we're just using
+# this hash to generate a random icon image if there's no other site icon available.
+# See: https://stackoverflow.com/a/52171480/20530257
+def cyrb53(theStr, seed=0):
+    h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for i in range(0, len(theStr):
+        ch = theStr.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+
 
 
 # Get a timestamp of when we started.
