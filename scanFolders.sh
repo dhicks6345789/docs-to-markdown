605446f8678027a5352815c0403e096137834fb7
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-02 10:24:32 +0100

    Create scanFolders.sh

---
+++ scanFolders.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -d .venv ]; then
+  python -m venv .venv
+  source .venv/bin/activate
+  pip install -r requirements.txt
+else
+  source .venv/bin/activate
+fi
+
+python3 scanFolders.py
